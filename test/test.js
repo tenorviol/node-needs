913fcd2f91699a2a0a3132ea8b42439396e61ad4
Chris Johnson <tenorviol@yahoo.com>
2011-06-13 21:23:40 -0700

    added io.get

--- test/test.js
+++ test/test.js
@@ -1,5 +1,23 @@
-var serverTest = require('../lib/serverTest'),
+var io = require('../lib/io'),
     connect = require('connect');
+
+exports['test get'] = function(assert) {
+  var server = connect()
+    .use(function(req, res, next) {
+      console.log('hello');
+      res.end('Boo');
+    });
+  
+  server.listen(3000, function() {
+    io.get('http://localhost:3000/', function(err, response) {
+      server.close();
+      assert.equal(200, response.statusCode);
+      assert.equal('Boo', response.body);
+      assert.done();
+    });
+  });
+};
+
 
 exports['test serverTest'] = function(assert) {
   var server = connect()
@@ -7,7 +25,7 @@ exports['test serverTest'] = function(assert) {
       res.end('Boo');
     });
   
-  serverTest(server, function(result) {
+  io.serverTest(server, function(result) {
     assert.equal(200, result.statusCode);
     assert.equal('Boo', result.body);
     assert.done();
