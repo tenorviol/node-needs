306117eb7d67d15089473b84b16a7cd21ccb77ab
Christopher Johnson <tenorviol@yahoo.com>
2011-08-10 15:16:01 -0700

    explicit indexes on collections

--- test/data.test.js
+++ test/data.test.js
@@ -1,26 +1,26 @@
 var data = require('../index').data;
 
-exports.testCollection = function (assert) {
+exports.testArrayCollection = function (assert) {
   var c = new data.ArrayCollection();
   
-  var o1 = { _id:1, foo: 'bar' };
-  c.add(o1);
-  assert.equal(o1, c.get(o1._id));
+  var o1 = { foo: 'bar' };
+  c.add(1, o1);
+  assert.equal(o1, c.get(1));
   
-  var o2 = { _id:2, foo: 'bar' };
-  c.add(o2);
-  assert.equal(o2, c.get(o2._id));
+  var o2 = { foo: 'bar' };
+  c.add(2, o2);
+  assert.equal(o2, c.get(2));
   
   assert.notEqual(c.get(1), c.get(2));
-  assert.equal(c.get(1).foo, c.get(2).foo);
+  assert.deepEqual(c.get(1), c.get(2));
   
-  var o3 = { _id:1000000, foo: 'fubar' };
-  c.add(o3);
-  assert.equal(o3, c.get(o3._id));
+  var o3 = { foo: 'fubar' };
+  c.add(1000000, o3);
+  assert.equal(o3, c.get(1000000));
   
-  c.remove(o1._id);
+  c.remove(1000000);
   try {
-    c.get(o1._id);
+    c.get(1000000);
     assert.ok(false);
   } catch (e) {
     // pass
